f765695b1b918d683deea48ae0738296884cfbc7
DronDrin <FunnyFunC@yandex.ru>
2025-06-20 08:49:11 +0300

    Add window system

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,7 +1,27 @@
 let gameSlider, difficultySlider;
 
+let currWindow;
+function openWindow(name) {
+   if (currWindow)
+      removeClass(currWindow, 'main__window_active');
+   currWindow = document.querySelector(`.main__window[data-window="${name}"]`)
+   addClass(currWindow, 'main__window_active');
+}
+
 window.addEventListener('load', () => {
    gameSlider = new Slider(document.getElementById('gameSlider'));
    difficultySlider = new Slider(document.getElementById('difficultySlider'));
-
+   currWindow = document.querySelector('.main__window_active');
+   openWindow('selector');
 });
+
+
+function addClass(el, className) {
+   if (!el.classList.contains(className))
+      el.classList.add(className);
+}
+
+function removeClass(el, className) {
+   if (el.classList.contains(className))
+      el.classList.remove(className);
+}
