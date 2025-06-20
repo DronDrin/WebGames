d02f056508d2c900732c2893a6e4e15ec48234b2
DronDrin <FunnyFunC@yandex.ru>
2025-06-20 11:48:08 +0300

    Add basic gameplay

--- js/game/minesweeper.js
+++ js/game/minesweeper.js
@@ -39,9 +39,117 @@ class Minesweeper extends Game {
             this.fieldEl.appendChild(cell);
         }
         this.resize();
+
+        this.fieldEl.addEventListener('mousedown', e => {
+            if (e.target.classList.contains('minesweeper__cell')) {
+                const cell = e.target;
+                const i = [...cell.parentNode.children].indexOf(cell);
+                this.cellClick(cell, Math.floor(i / this.height), i % this.height,
+                    e.button === 0);
+                e.preventDefault();
+            }
+        });
+        this.fieldEl.addEventListener('contextmenu', e => {
+            e.preventDefault();
+            return false;
+        })
+    }
+
+    cellClick(cell, x, y, open) {
+        if (cell.classList.contains('minesweeper__cell_open'))
+            return;
+        if (open) {
+            if (cell.classList.contains('minesweeper__cell_flag'))
+                return;
+            cell.classList.add('minesweeper__cell_open');
+            if (!this.field)
+                this.generateField(x, y);
+
+            const val = this.field[x][y];
+            if (val > 0 && val < 9) {
+                const numEl = document.createElement("div");
+                numEl.classList.add('minesweeper__num');
+                numEl.innerText = val;
+                cell.appendChild(numEl);
+            } else if (val === 9) {
+                this.lose();                // todo: beautiful lose animation
+            } else
+                this.propagateArea(x, y);
+        } else {
+            if (!this.field)
+                return;
+            if (cell.classList.contains('minesweeper__cell_flag')) {
+                cell.classList.remove('minesweeper__cell_flag');
+                cell.innerHTML = '';
+            }
+            else {
+                cell.classList.add('minesweeper__cell_flag');
+                const imgEl = document.createElement("img");
+                imgEl.classList.add('minesweeper__flag');
+                imgEl.src = 'img/minesweeper/flag.png';
+                cell.appendChild(imgEl);
+            }
+        }
+    }
+
+    propagateArea(x, y) {
+        setTimeout(() => {
+            this.getAround(x, y)
+                .forEach(c => {
+                    const cell = this.fieldEl.children[c.x * this.height + c.y];
+                    if (cell.classList.contains('minesweeper__cell_open'))
+                        return;
+                    cell.classList.add('minesweeper__cell_open');
+                    const val = this.field[c.x][c.y];
+                    if (val === 0)
+                        this.propagateArea(c.x, c.y);
+                    else if (val < 9) {
+                        const numEl = document.createElement("div");
+                        numEl.classList.add('minesweeper__num');
+                        numEl.innerText = val;
+                        cell.appendChild(numEl);
+                    }
+                })
+        }, 40);
     }
 
     generateField(firstX, firstY) {
+        do {
+            this.field = [];
+            for (let i = 0; i < this.width; i++)
+                this.field[i] = [];
+
+            let cords = [];
+            for (let i = 0; i < this.width; i++)
+                for (let j = 0; j < this.height; j++)
+                    cords.push({x: i, y: j});
+            cords = shuffleArray(cords);
+
+            for (let i = 0; i < this.mines; i++)
+                this.field[cords[i].x][cords[i].y] = 9;
+
+            for (let i = 0; i < this.width; i++)
+                for (let j = 0; j < this.height; j++) {
+                    if (this.field[i][j] !== 9)
+                        this.field[i][j] = this.getAround(i, j)
+                            .filter(c => this.field[c.x][c.y] === 9)
+                            .length;
+                }
+
+        } while (this.field[firstX][firstY] !== 0);
+    }
+
+    getAround(x, y) {
+        const around = [];
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue;
+                around.push({x: x + i, y: y + j});
+            }
+        }
+
+        return around.filter(c => c.x >= 0 && c.y >= 0 && c.x < this.width && c.y < this.height);
     }
 
     updateMinesLeft() {
@@ -71,5 +179,10 @@ class Minesweeper extends Game {
 
         this.fieldEl.style.width = target.width + 'px';
         this.fieldEl.style.height = target.height + 'px';
+    }
+
+
+    cleanup() {
+        this.fieldEl.innerHTML = '';
     }
 }
