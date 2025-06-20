8f59dd33460d24782ffff07e5f9f91537afd5e1c
DronDrin <FunnyFunC@yandex.ru>
2025-06-20 08:07:56 +0300

    Add slider auto-selecting first item on load

--- js/slider.js
+++ js/slider.js
@@ -6,15 +6,22 @@ class Slider {
         this.onchange = i => {};
         this.el.querySelector('.slider__body').addEventListener('mousedown', (e) => {
             if (e.target.classList.contains('slider__item')) {
-                const itemRect = e.target.getBoundingClientRect();
-                const sliderRect = this.el.getBoundingClientRect();
-                this.cursor.style.left = `${itemRect.x - sliderRect.x + this.el.scrollLeft - 10}px`;
-                this.cursor.style.top = `${itemRect.y - sliderRect.y + this.el.scrollTop}px`;
-                this.cursor.style.width = `${itemRect.width + 20}px`;
-                this.cursor.style.height = `${itemRect.height}px`;
-                this.chosen = Number(e.target.getAttribute('data-i'));
-                this.onchange(this.chosen);
+                this.change(e.target);
             }
         });
+        const firstItem = this.el.querySelector('.slider__item:first-child');
+        if (firstItem)
+            this.change(firstItem);
+    }
+
+    change(item) {
+        const itemRect = item.getBoundingClientRect();
+        const sliderRect = this.el.getBoundingClientRect();
+        this.cursor.style.left = `${itemRect.x - sliderRect.x + this.el.scrollLeft - 10}px`;
+        this.cursor.style.top = `${itemRect.y - sliderRect.y + this.el.scrollTop}px`;
+        this.cursor.style.width = `${itemRect.width + 20}px`;
+        this.cursor.style.height = `${itemRect.height}px`;
+        this.chosen = Number(item.getAttribute('data-i'));
+        this.onchange(this.chosen);
     }
 }
