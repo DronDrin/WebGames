56f9789a07a298960c581b766a08b79cb3ce1f5d
DronDrin <FunnyFunC@yandex.ru>
2025-06-20 08:59:26 +0300

    Add rules

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,27 +1,34 @@
 let gameSlider, difficultySlider;
 
 let currWindow;
+
 function openWindow(name) {
-   if (currWindow)
-      removeClass(currWindow, 'main__window_active');
-   currWindow = document.querySelector(`.main__window[data-window="${name}"]`)
-   addClass(currWindow, 'main__window_active');
+    if (currWindow)
+        removeClass(currWindow, 'main__window_active');
+    currWindow = document.querySelector(`.main__window[data-window="${name}"]`)
+    addClass(currWindow, 'main__window_active');
 }
 
 window.addEventListener('load', () => {
-   gameSlider = new Slider(document.getElementById('gameSlider'));
-   difficultySlider = new Slider(document.getElementById('difficultySlider'));
-   currWindow = document.querySelector('.main__window_active');
-   openWindow('selector');
+    gameSlider = new Slider(document.getElementById('gameSlider'));
+    difficultySlider = new Slider(document.getElementById('difficultySlider'));
+    currWindow = document.querySelector('.main__window_active');
+    openWindow('selector');
+
+    gameSlider.onchange = i => {
+        document.querySelectorAll('.main__rules')
+            .forEach(rules => removeClass(rules, 'main__rules_active'));
+        addClass(document.querySelector(`.main__rules:nth-child(${i + 1})`), 'main__rules_active');
+    };
 });
 
 
 function addClass(el, className) {
-   if (!el.classList.contains(className))
-      el.classList.add(className);
+    if (!el.classList.contains(className))
+        el.classList.add(className);
 }
 
 function removeClass(el, className) {
-   if (el.classList.contains(className))
-      el.classList.remove(className);
+    if (el.classList.contains(className))
+        el.classList.remove(className);
 }
